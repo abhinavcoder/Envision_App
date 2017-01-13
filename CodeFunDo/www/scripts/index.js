95de81e6f75d504888630914372cafbcdcf4a8ff
Mudit <mudit5bachhawat@gmail.com>
2017-01-13 14:38:40 +0530

    Camera from HTML page implemented

--- CodeFunDo/www/scripts/index.js
+++ CodeFunDo/www/scripts/index.js
@@ -1,30 +1,89 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿//// For an introduction to the Blank template, see the following documentation:
+//// http://go.microsoft.com/fwlink/?LinkID=397704
+//// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+//// and then run "window.location.reload()" in the JavaScript Console.
+//(function () {
+//    "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+//    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+//    function onDeviceReady() {
+//        // Handle the Cordova pause and resume events
+//        document.addEventListener( 'pause', onPause.bind( this ), false );
+//        document.addEventListener( 'resume', onResume.bind( this ), false );
         
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+//        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+//        var parentElement = document.getElementById('deviceready');
+//        var listeningElement = parentElement.querySelector('.listening');
+//        var receivedElement = parentElement.querySelector('.received');
+//        listeningElement.setAttribute('style', 'display:none;');
+//        receivedElement.setAttribute('style', 'display:block;');
+//    };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+//    function onPause() {
+//        // TODO: This application has been suspended. Save application state here.
+//    };
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+//    function onResume() {
+//        // TODO: This application has been reactivated. Restore application state here.
+//    };
+//})();
+
+var app = {
+    startCamera: function () {
+        CameraPreview.startCamera();
+    },
+
+    startCameraAnotherPos: function () {
+        CameraPreview.startCamera({ x: 50, y: 100, width: 300, height: 300, camera: "back", tapPhoto: true, previewDrag: true, toBack: false });
+    },
+
+    stopCamera: function () {
+        CameraPreview.stopCamera();
+    },
+
+    takePicture: function () {
+        CameraPreview.takePicture({ maxWidth: window.device.width, maxHeight: window.device.height });
+    },
+
+    switchCamera: function () {
+        CameraPreview.switchCamera();
+    },
+
+    show: function () {
+        CameraPreview.show();
+    },
+
+    hide: function () {
+        CameraPreview.hide();
+    },
+
+    colorEffectChanged: function () {
+        var effect = document.getElementById('colorEffectCombo').value;
+        CameraPreview.setColorEffect(effect);
+    },
+
+    init: function () {
+        document.getElementById('startCameraButton').addEventListener('click', this.startCamera, false);
+        document.getElementById('startCameraAnotherPosButton').addEventListener('click', this.startCameraAnotherPos, false);
+
+        document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
+        document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
+        document.getElementById('switchCameraButton').addEventListener('click', this.switchCamera, false);
+        document.getElementById('showButton').addEventListener('click', this.show, false);
+        document.getElementById('hideButton').addEventListener('click', this.hide, false);
+        document.getElementById('colorEffectCombo').addEventListener('change', this.colorEffectChanged, false);
+        //window.addEventListener('orientationchange', this.onStopCamera, false);
+
+        CameraPreview.setOnPictureTakenHandler(function (result) {
+            console.log(result);
+            document.getElementById('originalPicture').src = "data:image/jpeg;base64," + result; //originalPicturePath;
+            document.getElementById('previewPicture').src = result[1]; //previewPicturePath;
+        });
+    }
+};
+
+document.addEventListener('deviceready', function () {
+    app.init();
+    console.log("Readyyyyyyyyyyyyyyyyy.............");
+}, false);
